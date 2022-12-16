c44188fc7bc1019952e7375f6f27c9570112ac39
viniciusmkersten <120648584+viniciusmkersten@users.noreply.github.com>
2022-12-16 08:43:31 -0300

    Initial commit

---
+++ exemplo_github_1.py
@@ -0,0 +1,9 @@
+lista = []
+for recebe in range(2):
+    nome = input('Nome: ')
+    idade = int(input('idade: '))
+    profissao = input('prof: ')
+    lista.append(nome)
+    lista.append(idade)
+    lista.append(profissao)
+print(lista)
